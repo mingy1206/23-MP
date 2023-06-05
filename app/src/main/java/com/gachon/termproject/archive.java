90a46e7496b96763d8e03cf7f4566ffd65feae5e
mingy1206 <mingy837@gmail.com>
2023-06-06 07:30:22 +0900

    archive 완료& 모든 기능 구현완료

--- app/src/main/java/com/gachon/termproject/archive.java
+++ app/src/main/java/com/gachon/termproject/archive.java
@@ -3,16 +3,33 @@ package com.gachon.termproject;
 //출처 : https://wikidocs.net/99371
 //intent 참고
 //출처 : https://velog.io/@soyoung-dev/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%95%A1%ED%8B%B0%EB%B9%84%ED%8B%B0-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%A0%84%EB%8B%AC
+//permission check 정책변환
+//출처 : https://stackoverflow.com/questions/33472723/can-not-find-symbol-manifest-permission-write-external-storage-on-v23
+//permission 변경 정책변환 및 api 설정에 따른 변화
+//출처 : https://stackoverflow.com/questions/64221188/write-external-storage-when-targeting-android-10
+//수동 permission 방법
+//출처 : https://changwoos.tistory.com/717
+//이미지 업데이트
+//출처 : https://hwanschoi.tistory.com/128
 import static android.app.Activity.RESULT_OK;
 
-import android.animation.ObjectAnimator;
+
+import android.app.Application;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,11 +37,20 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class archive extends Fragment {
     private int imageViewCount = 0;
@@ -32,7 +58,9 @@ public class archive extends Fragment {
     private ImageView picture;
     private ImageView album;
     private RelativeLayout imageContainer;
-    private Button albumBtn, pictureBtn;
+    private Button albumBtn, pictureBtn,capturebtn;
+    private int size;
+    private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     Vibrator mVibe;
 
     public archive() {
@@ -52,12 +80,21 @@ public class archive extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_archive, container, false);
-
+        size =3;
         albumBtn =(Button)rootView.findViewById(R.id.album_btn);
         picture =(ImageView)rootView.findViewById(R.id.pictucre);
         pictureBtn=(Button) rootView.findViewById(R.id.picture_btn);
         imageContainer = (RelativeLayout) rootView.findViewById(R.id.imageContainer);
         mVibe = (Vibrator)getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        capturebtn=(Button)rootView.findViewById(R.id.capturebtn);
+
+
+        capturebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestStoragePermission();
+            }
+        });
         albumBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -130,14 +167,66 @@ public class archive extends Fragment {
     public void createNewImageView(String u) {
         album = new ImageView(requireContext());
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams
-                (this.imageContainer.getWidth()/4, this.imageContainer.getHeight()/4);
+                (this.imageContainer.getWidth()/size, this.imageContainer.getHeight()/size);
         album.setLayoutParams(layoutParams);
         Glide.with(this.imageContainer).load(u).into(album);
         imageContainer.addView(album);
         album.bringToFront();
         imageViewCount++;
     }
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else {
+            saveImage();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveImage();
+            } else {
+                Toast.makeText(getActivity(), "저장 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
+    private void saveImage() {
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/TermProject";
+        File directory = new File(path);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        // 파일 이름을 현재 시간으로 생성
+        String fileName = "image_" + System.currentTimeMillis() + ".jpg";
+        File file = new File(directory, fileName);
+
+        try {
+            Bitmap bitmap = Bitmap.createBitmap(imageContainer.getWidth(), imageContainer.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            imageContainer.draw(canvas);
+
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+            getActivity().sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+
+            Toast.makeText(getActivity(), "이미지가 저장되었습니다.", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.d("file 위치", file.getPath());
+            Toast.makeText(getActivity(), "파일을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getActivity(), "이미지 저장 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
 
