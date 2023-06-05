5e90d4b989dfa64a4d0dff3aa8c8dcafe6b283b8
mingy1206 <mingy837@gmail.com>
2023-06-06 03:09:57 +0900

    이미지 이동 완성

--- app/src/main/java/com/gachon/termproject/archive.java
+++ app/src/main/java/com/gachon/termproject/archive.java
@@ -1,16 +1,22 @@
 package com.gachon.termproject;
 //갤러리에서 이미지 가져오기
 //출처 : https://wikidocs.net/99371
+//intent 참고
+//출처 : https://velog.io/@soyoung-dev/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%95%A1%ED%8B%B0%EB%B9%84%ED%8B%B0-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%A0%84%EB%8B%AC
 import static android.app.Activity.RESULT_OK;
 
+import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -24,9 +30,11 @@ public class archive extends Fragment {
     private int imageViewCount = 0;
     public static String url;
     private ImageView picture;
+    private ImageView album;
     private RelativeLayout imageContainer;
-
     private Button albumBtn, pictureBtn;
+    Vibrator mVibe;
+
     public archive() {
         // Required empty public constructor
     }
@@ -49,6 +57,7 @@ public class archive extends Fragment {
         picture =(ImageView)rootView.findViewById(R.id.pictucre);
         pictureBtn=(Button) rootView.findViewById(R.id.picture_btn);
         imageContainer = (RelativeLayout) rootView.findViewById(R.id.imageContainer);
+        mVibe = (Vibrator)getActivity().getSystemService(Context.VIBRATOR_SERVICE);
         albumBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -66,7 +75,35 @@ public class archive extends Fragment {
                 intent.setType("image/*");
                 startActivityForResult(intent, 1);            }
         });
+        imageContainer.setOnTouchListener(new View.OnTouchListener() {
+            float previousX = 0;
+            float previousY = 0;
 
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int touchX = (int) event.getX();
+                        int touchY = (int) event.getY();
+
+                        float deltaX = touchX - previousX;
+                        float deltaY = touchY - previousY;
+
+                        // Move the album view based on the touch movement
+                        album.setTranslationX(album.getTranslationX() + deltaX);
+                        album.setTranslationY(album.getTranslationY() + deltaY);
+
+                        previousX = touchX;
+                        previousY = touchY;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return true;
+            }
+        });
         return rootView;
 
     }
@@ -91,15 +128,16 @@ public class archive extends Fragment {
         }
     }
     public void createNewImageView(String u) {
-        ImageView imageView = new ImageView(requireContext());
+        album = new ImageView(requireContext());
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams
-                (this.imageContainer.getWidth()/3, this.imageContainer.getHeight()/3);
-        imageView.setLayoutParams(layoutParams);
-        Glide.with(this.imageContainer).load(u).into(imageView);
-        imageContainer.addView(imageView);
-        imageView.bringToFront();
+                (this.imageContainer.getWidth()/4, this.imageContainer.getHeight()/4);
+        album.setLayoutParams(layoutParams);
+        Glide.with(this.imageContainer).load(u).into(album);
+        imageContainer.addView(album);
+        album.bringToFront();
         imageViewCount++;
     }
+
 
 }
 
